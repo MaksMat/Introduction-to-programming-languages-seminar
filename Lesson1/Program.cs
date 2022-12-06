c00705936cdeba2fd7e673c7aaa16c7c27f442c4
Maks Matv <maks-mat@ya.ru>
2022-12-06 11:22:10 +0700

    Добавили пример

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+
+int resultNumber = number * number;
+
+Console.WriteLine(resultNumber);
